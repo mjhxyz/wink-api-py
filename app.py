9f02c5c82c32b5c9f444cb435ef812d29eee05c9
Mao <mjhxyz@foxmail.com>
2023-07-09 11:09:19 +0800

    add: setup.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.get('/')
 def hello():
     return "Hello World!"
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='wink',
+    version='0.0.1',
+    description='A general-purpose management system background api',
+    author='Mao',
+    author_email='mjhxyz@foxmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'Flask==2.1.2',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Operating System :: OS Independent',
+    ],
+)
